2918fa082e0367464821c08eda7d6750f2186645
Guido Josquin <g.josquin+git@gmail.com>
2018-03-02 16:21:01 +0100

    Change example to illustrate SseEmitter issues

--- src/main/java/com/connectionleak/demo/ConnectionLeakController.java
+++ src/main/java/com/connectionleak/demo/ConnectionLeakController.java
@@ -13,17 +13,14 @@ public class ConnectionLeakController {
 
   private static final long SSE_TIMEOUT_MS = 60_000L;
 
-  private final DerpService derpService;
   private final SseObservable obs;
 
-  ConnectionLeakController(DerpService derpService) {
-    this.derpService = derpService;
+  ConnectionLeakController() {
     obs = new SseObservable(SSE_TIMEOUT_MS);
   }
 
   @GetMapping("")
   public SseEmitter home() {
-    derpService.derp();
     return obs.subscribeSse();
   }
 
